906175c6d046f87fd5428ccead570adf5c1c4456
MStee09 <michael@gorocketshipping.com>
2025-11-06 11:23:03 -0600

    Updated calendar.jsx

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -28,10 +28,10 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "grid grid-cols-7",
+        head_row: "!grid !grid-cols-7 w-full",
         head_cell:
-          "text-muted-foreground rounded-md font-normal text-[0.8rem] flex items-center justify-center",
-        row: "grid grid-cols-7 w-full mt-2",
+          "text-muted-foreground rounded-md font-normal text-[0.8rem] flex items-center justify-center h-9",
+        row: "!grid !grid-cols-7 w-full mt-2",
         cell: cn(
           "text-center text-sm p-0 relative flex items-center justify-center [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
           props.mode === "range"
