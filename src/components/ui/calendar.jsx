639d4aca77375346ccd16a5ead29350a40a8a306
MStee09 <michael@gorocketshipping.com>
2025-11-06 11:04:46 -0600

    Updated calendar.jsx

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -28,9 +28,9 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex gap-1",
+        head_row: "flex w-full gap-1",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex items-center justify-center",
+          "text-muted-foreground rounded-md flex-1 font-normal text-[0.8rem] flex items-center justify-center",
         row: "flex w-full mt-2 gap-1",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md flex-1",
@@ -40,7 +40,7 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-full p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
